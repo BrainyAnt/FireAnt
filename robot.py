c1ed0ec9e2c5b667c23d2d5b4b54b26799e87097
Andrei <andrei.brumboiu@gmail.com>
2018-02-01 12:42:12 +0200

    Added help comments

--- robot.py
+++ robot.py
@@ -4,6 +4,8 @@
 from fireant import FireAnt
 import random
 
+# GPIO setup
+
 # User implemented functions
 
 def userFunctions(args):
@@ -12,44 +14,53 @@ def userFunctions(args):
     return value
 
 def readSensor1():
+    #get sensor data
     return 100
 
 def readSensor2():
+    #another user defined sensor reading function
     return random.randint(1,501)
 
 # main part
 if __name__ == '__main__':
     try:
-        #create a FireAnt object. This registers with the platform and sets the robot online.
+        # STEP1 create a FireAnt object. This registers with the platform and sets the robot online.
         myAnt = FireAnt('auth.json')
+        
+        # STEP2 (optional) get information about your robot
         print(myAnt.is_robot_online())
         print(myAnt.get_name())
         print(myAnt.get_description())
+        
         while myAnt.is_robot_online():
             
-            #wait for a user to request control
+            # STEP4 wait for a user to request control
             print('Waiting for users ...')
             myAnt.wait_for_available_user()
             print('Got user!')
-            #as long as the user is online and in control:
+            
+            # as long as the user is online and in control:
             while myAnt.get_useron():
-                #get control data
+                # STEP5 get control data
                 controldata = myAnt.get_control_data()
-                print(controldata)
                 
-                #do stuff with control data
-                #check if sensor reading is requested
+                # STEP6 do stuff with control data
+                userFunctions(controldata)
+                
+                # STEP7 handle sensors
+                # check if sensor reading is requested
                 request1 = myAnt.get_sensor_request('sensor1')
                 request2 = myAnt.get_sensor_request('sensor_2')
                 
-                #send sensor reading
+                # send sensor reading to database
                 if request1:
                     reading = readSensor1()
                     myAnt.publish_data( ("sensor1", reading, False) )
+                    #myAnt.publish_data( (SENSOR_NAME, READING, REQUEST) )
                 if request2:
                     myAnt.publish_data(("sensor_2", readSensor2(), True))
             
-            #log session
+            # STEP8 log session
             myAnt.log_session()
     except KeyboardInterrupt:
         myAnt.log_session()
